b264949e418a2d08e2e7ebf3a21a06b7d029541b
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-12-18 07:53:50 +0530

    creating the front-end

---
+++ client/src/components/demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Demo() {
+  return (
+    <div className='npc-demo'>
+        
+    </div>
+  )
+}
+
+export default Demo
---
+++ client/src/pages/home-page/home-page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Demo from "../../components/demo";
+
+function HomePage() {
+  return <div>
+    <Demo/>
+  </div>;
+}
+
+export default HomePage;
