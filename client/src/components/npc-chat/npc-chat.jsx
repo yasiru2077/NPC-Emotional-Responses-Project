71050ebc0c1b6075297caa61a3b5bae905404c23
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-12-19 16:07:14 +0530

    creating the chat bot

--- client/src/components/npc-chat/npc-chat.jsx
+++ client/src/components/npc-chat/npc-chat.jsx
@@ -1,31 +1,46 @@
-import React from 'react'
-import "./npc-chat.css"
+import React from "react";
+import "./npc-chat.css";
 
 function NpcChat() {
   return (
-    <section className='chat-bot-container'>
-        <div className='chat-bot'>
-            <h1>Maren the fishermen v1.0</h1>
-            <div>
-            <form onSubmit="">
-            <div className="">
-              <input
-                type="text"
-                className=""
-                placeholder="ENTER MESSAGE..."
-              />
-              <button 
-                type="submit"
-                className=""
-              >
-                SEND
-              </button>
-            </div>
-          </form>
-            </div>
+    <section className="chat-bot-container">
+      <div className="chat-bot">
+        <h1>Maren the fishermen v1.0</h1>
+        <div className="chat-function-container">
+          <div className="text-showcase">
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. It is a
+              long established fact that a reader will be distracted by the
+              readable content of a page when looking at its layout.
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. It is a
+              long established fact that a reader will be distracted by the
+              readable content of a page when looking at its layout.
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+            </p>
+          </div>
+          <div>
+            <form onSubmit="" className="user-responses-form">
+              <div className="">
+                <input
+                  type="text"
+                  className=""
+                  placeholder="ENTER MESSAGE..."
+                />
+                <button type="submit" className="">
+                  Enter
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default NpcChat
+export default NpcChat;
